4954d6a2cd8f080c5fcf65ba4940ef7b1db0dcf2
Eric Pierce <wapcaplet88@gmail.com>
2014-01-01 22:50:54 -0700

    Tabs to spaces

--- SoundTest/src/com/simonmonk/spike/Main.java
+++ SoundTest/src/com/simonmonk/spike/Main.java
@@ -12,32 +12,32 @@ import android.widget.EditText;
 public class Main extends Activity {
 
 
-	/** Called when the activity is first created. */
-	@Override
+  /** Called when the activity is first created. */
+  @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        
+
         final EditText field = (EditText)findViewById(R.id.numberField);
-        
+
         Button button = (Button)findViewById(R.id.playButton);
         button.setOnClickListener(
-        		new OnClickListener()
-        		{
-					public void onClick(View v) 
-					{
-						try 
-						{
-							int i = Integer.parseInt(field.getText().toString(), 16);
-							Log.i("SRM", "i=" + i);
-							Beeper b = new Beeper();
-							b.beep(i);
-						} 
-						catch (NumberFormatException e) 
-						{
-							field.setText("Err");
-						}
-					}
-        		});
+            new OnClickListener()
+            {
+          public void onClick(View v)
+          {
+            try
+            {
+              int i = Integer.parseInt(field.getText().toString(), 16);
+              Log.i("SRM", "i=" + i);
+              Beeper b = new Beeper();
+              b.beep(i);
+            }
+            catch (NumberFormatException e)
+            {
+              field.setText("Err");
+            }
+          }
+            });
     }
-} 
+}
