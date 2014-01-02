7a6ff9992e4d72d1913d09fd92b8f05c17b36b10
Eric Pierce <wapcaplet88@gmail.com>
2014-01-02 16:09:05 -0700

    Strip trailing whitespace

--- DroidLightDisplayLaunch/src/org/cbase/blinkendroid/audio/AndroidAudioDevice.java
+++ DroidLightDisplayLaunch/src/org/cbase/blinkendroid/audio/AndroidAudioDevice.java
@@ -1,8 +1,8 @@
 /*
  * TODO prüfen, ob wir diese Datei auf GPLv3 upgraden können
- * 
+ *
  * org.hermit.android.io: Android utilities for accessing peripherals.
- * 
+ *
  * These classes provide some basic utilities for accessing the audio
  * interface, at present.
  *
@@ -11,7 +11,7 @@
  * <p>This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2
  * as published by the Free Software Foundation (see COPYING).
- * 
+ *
  * <p>This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -31,30 +31,30 @@ public class AndroidAudioDevice
 {
    AudioTrack track;
    short[] buffer = new short[1024];
- 
+
    public AndroidAudioDevice( )
    {
-      int minSize =AudioTrack.getMinBufferSize( 8000, AudioFormat.CHANNEL_CONFIGURATION_MONO, AudioFormat.ENCODING_PCM_16BIT );        
-      track = new AudioTrack( AudioManager.STREAM_MUSIC, 8000, 
-                                        AudioFormat.CHANNEL_CONFIGURATION_MONO, AudioFormat.ENCODING_PCM_16BIT, 
+      int minSize =AudioTrack.getMinBufferSize( 8000, AudioFormat.CHANNEL_CONFIGURATION_MONO, AudioFormat.ENCODING_PCM_16BIT );
+      track = new AudioTrack( AudioManager.STREAM_MUSIC, 8000,
+                                        AudioFormat.CHANNEL_CONFIGURATION_MONO, AudioFormat.ENCODING_PCM_16BIT,
                                         minSize, AudioTrack.MODE_STREAM);
-      track.play();        
-   }       
- 
+      track.play();
+   }
 
-   public void writeSamples(float[] samples) 
-   {    
+
+   public void writeSamples(float[] samples)
+   {
       fillBuffer( samples );
       track.write( buffer, 0, samples.length );
 //      Log.d("sound", "write");
    }
- 
+
    private void fillBuffer( float[] samples )
    {
       if( buffer.length < samples.length )
          buffer = new short[samples.length];
- 
+
       for( int i = 0; i < samples.length; i++ )
          buffer[i] = (short)(samples[i] * Short.MAX_VALUE);;
-   }            
+   }
 }
