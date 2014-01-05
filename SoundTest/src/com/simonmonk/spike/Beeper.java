bb718dbb0cb08b1cbff430db2de164804077e1b7
Eric Pierce <wapcaplet88@gmail.com>
2014-01-04 19:07:29 -0700

    Total refactor of Beeper
    Rather than generating the full audio clip (16 pulses of 1000 samples
    each) in a single go, buffer the `zero` and `one` pulses and emit them
    in order.
    
    Also add a dummy "header" pulse, to work around a problem I've had with
    my circuit and/or Arduino not reading the first pulse correctly. This
    goes along with a corresponding change to the arduino code (incoming in
    a future commit) to detect and ignore the longer header pulse.

--- SoundTest/src/com/simonmonk/spike/Beeper.java
+++ SoundTest/src/com/simonmonk/spike/Beeper.java
@@ -6,64 +6,78 @@ import android.media.AudioTrack;
 
 public class Beeper {
 
+  // Number of samples per second of audio
   private final static int SAMPLE_RATE = 16000;
 
-  private final static int ONE_DURATION = 32;
-  private final static int ZERO_DURATION = 8;
-  private final static int BIT_DURATION = 64;
-  private final static int DURATION = BIT_DURATION * 32;
-  private final static float f = 1000.0f;
+  // Number of samples to use for encoding bits
+  private final static int BIT_SAMPLES = 1000;
+  private final static int ZERO_SAMPLES = 10;
+  private final static int ONE_SAMPLES = 200;
+  private final static int HEADER_SAMPLES = 500;
+  
+  // Tone properties
+  private final static float FREQ = 1000.0f;
+  private final static float AMPLITUDE = 32000.0f;
 
-  private short[] buffer = null;
-
+  // Emit a series of tone pulses, encoding `word`,
+  // with long beeps for 1 and short beeps for 0.
   public void beep(int word) {
     AudioTrack at;
-    int bufsizbytes = DURATION * SAMPLE_RATE / 1000;
-    int bufsizsamps = bufsizbytes / 2;
-    buffer = new short[bufsizsamps];
-    fillbuf(word, bufsizsamps);
-    try {
-      at = new AudioTrack(AudioManager.STREAM_MUSIC, SAMPLE_RATE,
-          AudioFormat.CHANNEL_CONFIGURATION_MONO,
-          AudioFormat.ENCODING_PCM_16BIT, bufsizbytes,
-          AudioTrack.MODE_STATIC);
-      at.setStereoVolume(1.0f, 1.0f);
-      at.write(buffer, 0, bufsizsamps);
-      at.play();
-    } catch (IllegalArgumentException e) {
-      e.printStackTrace();
+    int total_samples = BIT_SAMPLES * 16;
+    int bit;
+
+    // Precalculate the buffers
+    short[] header_buffer = pulse_buffer(BIT_SAMPLES, HEADER_SAMPLES);
+    short[] one_buffer = pulse_buffer(BIT_SAMPLES, ONE_SAMPLES);
+    short[] zero_buffer = pulse_buffer(BIT_SAMPLES, ZERO_SAMPLES);
+
+    // Create a streaming AudioTrack
+    at = new AudioTrack(AudioManager.STREAM_MUSIC, SAMPLE_RATE,
+        AudioFormat.CHANNEL_CONFIGURATION_MONO,
+        AudioFormat.ENCODING_PCM_16BIT, total_samples*2,
+        AudioTrack.MODE_STREAM);
+    at.setStereoVolume(1.0f, 1.0f);
+
+    // Begin streaming
+    at.play();
+    
+    // Write a dummy header, in case the first pulse doesn't make it through
+    at.write(header_buffer, 0, BIT_SAMPLES);
+    
+    // Write pulses for each bit in the word, starting with
+    // the most significant bit
+    for (int i = 15; i >= 0; i--) {
+      bit = ((word >> i) & 1);
+      if (bit == 0) {
+        at.write(zero_buffer, 0, BIT_SAMPLES);
+      }
+      else {
+        at.write(one_buffer, 0, BIT_SAMPLES);
+      }
     }
 
+    // Stop streaming
+    at.stop();
   }
-
-  void fillbuf(int word, int bufsizsamps) {
-    double omega, t;
+  
+  // Build an audio buffer representing an audio pulse, having `total_samples` in all,
+  // with the first `pulse_samples` being a sine wave, and the remaining samples being silence.
+  public short[] pulse_buffer(int total_samples, int pulse_samples) {
+    short[] buffer = new short[total_samples];
+    double omega = (float) (2.0 * Math.PI * FREQ);
+    double t = 0.0;
     double dt = 1.0 / SAMPLE_RATE;
-    t = 0.0;
-    omega = (float) (2.0 * Math.PI * f);
-    for (int i = 0; i < bufsizsamps; i++) {
-      if (toneRequired(t, word)) {
-        buffer[i] = (short) (32000.0 * Math.sin(omega * t));
-      } else {
+
+    for (int i = 0; i < total_samples; i++) {
+      if (i < pulse_samples) {
+        buffer[i] = (short) (AMPLITUDE * Math.sin(omega * t));
+      }
+      else {
         buffer[i] = 0;
       }
       t += dt;
     }
-  }
-
-  boolean toneRequired(double t, long word) {
-    int ms = (int) (t * 1000);
-    int bitIndex = ms / BIT_DURATION;
-    int bit = (int) ((word >> (15 - bitIndex)) & 1);
-    int msWithinBit = ms - (bitIndex * BIT_DURATION);
-
-    if (bit == 1 && msWithinBit < ONE_DURATION) {
-      return true;
-    }
-    if (bit == 0 && msWithinBit < ZERO_DURATION) {
-      return true;
-    }
-    return false;
+    return buffer;
   }
 
 }
--- SoundTest/src/com/simonmonk/spike/Main.java
+++ SoundTest/src/com/simonmonk/spike/Main.java
@@ -14,30 +14,29 @@ public class Main extends Activity {
 
   /** Called when the activity is first created. */
   @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.main);
 
-        final EditText field = (EditText)findViewById(R.id.numberField);
+    final EditText field = (EditText)findViewById(R.id.numberField);
 
-        Button button = (Button)findViewById(R.id.playButton);
-        button.setOnClickListener(
-            new OnClickListener()
-            {
-          public void onClick(View v)
-          {
-            try
-            {
-              int i = Integer.parseInt(field.getText().toString(), 16);
-              Log.i("SRM", "i=" + i);
-              Beeper b = new Beeper();
-              b.beep(i);
-            }
-            catch (NumberFormatException e)
-            {
-              field.setText("Err");
-            }
-          }
-            });
-    }
+    final Beeper beeper = new Beeper();
+
+    Button button = (Button)findViewById(R.id.playButton);
+    button.setOnClickListener(new OnClickListener() {
+      public void onClick(View v)
+      {
+        try
+        {
+          int i = Integer.parseInt(field.getText().toString(), 16);
+          Log.i("SRM", "i=" + i);
+          beeper.beep(i);
+        }
+        catch (NumberFormatException e)
+        {
+          field.setText("Err");
+        }
+      }
+    });
+  }
 }
